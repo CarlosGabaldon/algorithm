4de579df02b5bdc7e826fe9fda134bb60f54c40d
CarlosGabaldon <cgabaldon@gmail.com>
2018-05-21 13:22:37 -0700

    Test for no dups and array/slice greater than 10 capacity

--- duplicates/main_test.go
+++ duplicates/main_test.go
@@ -8,6 +8,19 @@ func TestDups(t *testing.T) {
 	slice := []int{2, 3, 2}
 	result := Dups(slice)
 	if result != true {
-		t.Errorf("expecting true, got %t ", result)
+		t.Errorf("expecting true, got %t", result)
 	}
+
+	slice = []int{3, 4, 5, 6}
+	result = Dups(slice)
+	if result != false {
+		t.Errorf("expecting false, got %t", result)
+	}
+
+	slice = []int{2, 3, 7, 4, 11, 100, 99, 12, 34, 899}
+	result = Dups(slice)
+	if result != false {
+		t.Errorf("expecting false, got %t", result)
+	}
+
 }
