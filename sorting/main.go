061981ff12c2bcdbb5ffee560dd7024b45bddf96
CarlosGabaldon <cgabaldon@gmail.com>
2018-05-21 19:10:31 -0700

    Sorting fun

---
+++ sorting/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func SecondHighest(numbers []int) int {
+	sort.Ints(numbers)
+	return numbers[len(numbers)-2]
+
+}
+func main() {
+	numbers := []int{2, 4, 1, 60, 3, 40}
+	fmt.Println("Given the numbers: ", numbers)
+	fmt.Println("Find the second highest number")
+	secondHighest := SecondHighest(numbers)
+	fmt.Println("Second highest number: ", secondHighest)
+}
---
+++ sorting/main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSecondHighest(t *testing.T) {
+	slice := []int{2, 33, 8, 1, 10, 12}
+	result := SecondHighest(slice)
+	if result != 8 {
+		t.Errorf("expecting 8, but got %d", result)
+	}
+}
