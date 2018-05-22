d49a4b76da02f7a08388b3513f7b49dfb7383aae
CarlosGabaldon <cgabaldon@gmail.com>
2018-05-21 19:15:28 -0700

    Make sure your test data is correct before committing ;)

--- sorting/main_test.go
+++ sorting/main_test.go
@@ -7,7 +7,7 @@ import (
 func TestSecondHighest(t *testing.T) {
 	slice := []int{2, 33, 8, 1, 10, 12}
 	result := SecondHighest(slice)
-	if result != 8 {
+	if result != 12 {
 		t.Errorf("expecting 8, but got %d", result)
 	}
 }
